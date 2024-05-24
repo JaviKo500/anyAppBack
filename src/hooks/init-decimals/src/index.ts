512ed8f175ea0b69d2b177f942df40fcb7541b8c
JaViKo500 <javikogutierrez64@gmail.com>
2024-05-24 15:35:06 -0500

    check test

--- src/hooks/init-decimals/src/index.ts
+++ src/hooks/init-decimals/src/index.ts
@@ -1,8 +1,6 @@
 import { defineHook } from '@directus/extensions-sdk';
 
 export default defineHook(({ init }) => {
-	console.log('<--------------- JK Index --------------->');
-	console.log('test');
 	init('routes.before',() => {
 		const pg = require('pg');
 		pg.types.setTypeParser(1700, (value: string) => {
